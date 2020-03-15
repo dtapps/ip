1070d6cb689b73baf1d85b61831c94f2b1d5eb18
Chaim <gc@dtapp.net>
2020-03-15 11:58:27 +0800

    修复获取当前客户端IP地址

--- src/Ip.php
+++ src/Ip.php
@@ -19,27 +19,14 @@ class Ip extends Client
      */
     protected function get()
     {
-        static $ip = null;
-        if ($ip !== null)
-            return $ip;
+        $ip = '0.0.0.0';
         if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            //为了兼容百度的CDN，所以转成数组
             $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            $pos = array_search('unknown', $arr);
-            if (false !== $pos)
-                unset($arr[$pos]);
-            $ip = trim($arr[0]);
-        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-        if (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip = $arr[0];
+        } else {
             $ip = $_SERVER['REMOTE_ADDR'];
-        } else
-            $ip = '0.0.0.0';
-        if ($ip == '::1')  //localhost开启IPv6时的特殊分处理
-            $ip = '127.0.0.1';
-        // IP地址合法验证
-        $long = sprintf("%u", ip2long($ip));
-        $ip = $long ? $ip : '0.0.0.0';
+        }
         return $ip;
     }
 }
